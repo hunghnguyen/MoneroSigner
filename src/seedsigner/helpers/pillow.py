90493f941f0abc1cadecdbbd4c75b4e3dae7e21a
who asks? <user@localhost>
2024-06-07 14:52:49 -0600

    fix all uses of ImageFont.getsize

---
+++ src/seedsigner/helpers/pillow.py
@@ -0,0 +1,10 @@
+from PIL import __version__ as pil_version
+
+
+PILLOW_VERSION = pil_version.split('.')
+
+def get_font_size(font: ImageFont, text: str) -> Tuple[int, int]:  # width, height
+    if int(PILLOW_VERSION[0]) < 10:
+        return font.getsize(text)
+    box = font.getbbox(text)
+    return (font.getlength(text), box[3] - box[1])
