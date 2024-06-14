09ea94b5892f4fb83486e1730e49c030a7a398bd
who asks? <user@localhost>
2024-06-14 17:44:36 -0600

    automatic version bumping, missing to update version in setup.py automaticaly!

---
+++ tools/increment_version.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from re import search
+
+class VersionUpdater:
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.current_version = self.get_current_version()
+
+    def get_current_version(self):
+        with open(self.file_path, 'r') as file:
+            for line in file:
+                if line.startswith('    VERSION ='):
+                    return search(r'\d+\.\d+\.\d+', line).group()
+
+    def update_version(self, new_version):
+        with open(self.file_path, 'r') as file:
+            lines = file.readlines()
+        with open(self.file_path, 'w') as file:
+            for line in lines:
+                if line.startswith('    VERSION ='):
+                    line = f'    VERSION = "{new_version}"\n'
+                file.write(line)
+
+    def increment_version(self, major=False, minor=False, patch=False):
+        major_num, minor_num, patch_num = map(int, self.current_version.split('.'))
+        
+        if major:
+            new_version = f'{major_num + 1}.0.0'
+        elif minor:
+            new_version = f'{major_num}.{minor_num + 1}.0'
+        else:
+            new_version = f'{major_num}.{minor_num}.{patch_num + 1}'
+        
+        self.update_version(new_version)
+        print(f'Version updated to {new_version}')
+
+def main():
+    parser = ArgumentParser(description='Increment version in controller.py')
+    parser.add_argument('--major', action='store_true', help='Increment major version')
+    parser.add_argument('--minor', action='store_true', help='Increment minor version')
+    parser.add_argument('--patch', action='store_true', help='Increment patch version')
+    
+    args = parser.parse_args()
+    
+    updater = VersionUpdater('src/seedsigner/controller.py')
+    updater.increment_version(args.major, args.minor, args.patch)
+
+if __name__ == '__main__':
+    main()
---
+++ tools/tag_version.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from re import search
+from subprocess import run
+
+class VersionTagger:
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.current_version: List[int] = [int(v) for v in self.get_current_version().split('.')]
+
+    def get_current_version(self) -> None:
+        with open(self.file_path, 'r') as file:
+            for line in file:
+                if line.startswith('    VERSION ='):
+                    return search(r'\d+\.\d+\.\d+', line).group()
+
+    def version_short(self) -> str:
+        version = self.current_version.copy()
+        if version[2] == 0:
+            del version[2]
+            v = [str(i) for i in version]
+        return f"v{'.'.join(v)}"
+
+    def exec(self, commit: bool, push: bool) -> None:
+        # try:
+        self.add()
+        if commit:
+            self.commit()
+        if push:
+            self.push()
+        # except Exception as e:
+        #    print(e)
+
+    def add(self) -> None:
+        cmd = f'git add {self.file_path}'
+        run(cmd, shell=True)
+
+    def commit(self) -> None:
+        version = [str(i) for i in self.current_version]
+        msg = f"updated version in {self.file_path} to {'.'.join(version)}"
+        cmd = f'git commit -m "{msg}"'
+        run(cmd, shell=True)
+
+    def push(self) -> None:
+        cmd = 'git push --tags origin master'
+        run(cmd, shell=True)
+
+
+def main():
+    parser = ArgumentParser(description='Increment version in controller.py')
+    parser.add_argument('--no-commit', dest='commit', action='store_false', default=True, help='Do not commit')
+    parser.add_argument('--no-push', dest='push', action='store_false', default=True, help='Do not commit')
+    
+    args = parser.parse_args()
+    
+    tagger = VersionTagger('src/seedsigner/controller.py')
+    tagger.exec(args.commit, args.push)
+
+if __name__ == '__main__':
+    main()
