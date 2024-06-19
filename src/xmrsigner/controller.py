e9b5296296aa5aa2510ae85538ddc590bfc0e620
who asks? <user@localhost>
2024-06-19 09:53:59 -0600

    updated version in src/xmrsigner/controller.py to 0.3.2

--- src/xmrsigner/controller.py
+++ src/xmrsigner/controller.py
@@ -51,7 +51,7 @@ class Controller(Singleton):
         rather than at the top in order avoid circular imports.
     """
 
-    VERSION = "0.3.1"
+    VERSION = "0.3.2"
 
     buttons: HardwareButtons = None
     storage: SeedStorage = None
